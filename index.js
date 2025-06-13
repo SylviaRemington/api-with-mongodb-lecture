2c1a5c240c3e3e2dbb1f734fd830ead70b6e702c
SylviaRemington <sylviaremington@gmail.com>
2025-06-13 00:09:06 -0700

    adding notes explaining what is going on with the sections of index.js for future reference

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = express();
 const mongoose = require("mongoose");
 const Chicken = require("./models/chicken");//importing my models
 
-app.post("/chicken-types", async (req, res) => {
+app.post("/chicken-types", async (req, res) => { //this doesn't do anything until we trigger the code & we do that with postman
   res.send("this will create a new chicken type");
   // TODO: Make this request dynamic so a user can use a tool such as postman
   // to send data to the API for saving in the database
@@ -27,14 +27,14 @@ app.get("/chicken-types", async (req, res) => {
   res.send(chickensAsHtml);
 });
 
-app.listen(process.env.PORT, async () => {
-  try {
-    await mongoose.connect(process.env.MONGODB_URI);
-    console.log("Connected to MongoDB Atlas");
-  } catch (e) {
-    console.error("A problem occured connecting", e);
-  }
-});
+// app.listen(process.env.PORT, async () => {
+//   try {
+//     await mongoose.connect(process.env.MONGODB_URI);
+//     console.log("Connected to MongoDB Atlas");
+//   } catch (e) {
+//     console.error("A problem occured connecting", e);
+//   }
+// });
 
 
 //THIS WAS WRITTEN EARLY ON IN THE LECTURE & THEN CHANGED
@@ -44,10 +44,12 @@ app.listen(process.env.PORT, async () => {
 
 //connecting to the database when we start the api
 //adding a try and a catch, and also adding async function:
-app.listen(process.env.PORT, async () =>
+app.listen(process.env.PORT, async () => {//app starts and we try to connect to the database
   try {
-
-  } catch (e) {
+    await mongoose.connect(process.env.MONGODB_URI);
+    console.log('Connected to MongoDB Atlas'); //if works and connects, this will be the message
+  } catch (e) { //if doesn't work and doesn't connect, this will be the error message below
+    console.error('A problem occurred connecting', e);
 
   }
-);
+});
