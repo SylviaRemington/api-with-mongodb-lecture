b6500118b13ad42fad01721c066540e62eb15068
SylviaRemington <sylviaremington@gmail.com>
2025-06-12 22:59:47 -0700

    adding notes to the model of chicken.js - defining the shape of the data

--- models/chicken.js
+++ models/chicken.js
@@ -1,11 +1,12 @@
-const mongoose = require("mongoose");
+const mongoose = require("mongoose"); //package needed to create a model, so require it
 
-const chickenSchema = new mongoose.Schema({
-  name: { type: String, required: true, unique: true },
+const chickenSchema = new mongoose.Schema({ //defining the Schema
+  name: { type: String, required: true, unique: true }, // now have to define the keys and datatypes for a chicken 
   color: { type: String, required: true },
   flavourRating: { type: Number, min: 0, max: 10, required: true },
 });
 
-const Chicken = mongoose.model("Chicken", chickenSchema);
+const Chicken = mongoose.model("Chicken", chickenSchema); //need to export the model
+// creating the model
 
-module.exports = Chicken;
+module.exports = Chicken; //need to export the model
